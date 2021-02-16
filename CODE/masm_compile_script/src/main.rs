b1f4dabc39f73713fb82a447d43e9e39b31030c1
Connor Slade <turtleslade@gmail.com>
2021-02-16 00:51:46 -0500

    Space

--- CODE/masm_compile_script/src/main.rs
+++ CODE/masm_compile_script/src/main.rs
@@ -1,11 +1,12 @@
 // Made by Connor Slade (Sigma#8214 on Discord) (https://github.com/Basicprogrammer10 on Github)
 use std::env;
 use std::process::exit;
+
 mod help;
 //use help;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();let args_len = args.len();
+    let args: Vec<String> = env::args().collect();
 
     print!("\x1B[2J\x1B[1;1H");
     println!("{}", color_print("                                         MASM32-Compile-Script                                        ", 45));
@@ -19,23 +20,25 @@ fn main() {
     return;
 }
 
-fn compile_script(file_location: &str){
+fn compile_script(file_location: &str) {
     println!("        {}        ", color_print("                                      COMPILING                                       ", 44));
+    println!("{}", file_location);
 }
 
-fn parse_args(args:Vec<String>){
+fn parse_args(args: Vec<String>) {
     let args_len = args.len();
 
     if args_len <= 1 {
         help::show_help();
         exit(0);
-    }else if args_len == 2 {
+    } else if args_len == 2 {
         compile_script(&*args[1]);
     }
 }
+
 fn color_print(text: &str, color_index: i32) -> String {
     let output = ["[", &color_index.to_string()[..], "m", text, "[0m"].join("");
     return output;
 }
-///TODO:Add Compiling / Linking
-///TODO:Less Bad Arg Parsing???
+//TODO:Add Compiling / Linking
+//TODO:Less Bad Arg Parsing???
